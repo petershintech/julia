4977d52a116ed517080c7978342ff082f5eb99c4
Jeff Bezanson <jeff.bezanson@gmail.com>
2021-01-08 16:01:51 -0500

    improve performance of disabling finalizers in locks
    helps #38947

--- src/ccall.cpp
+++ src/ccall.cpp
@@ -1481,6 +1481,22 @@ static jl_cgval_t emit_ccall(jl_codectx_t &ctx, jl_value_t **args, size_t nargs)
         tbaa_decorate(tbaa_const, tid);
         return mark_or_box_ccall_result(ctx, tid, retboxed, rt, unionall, static_rt);
     }
+    else if (is_libjulia_func(jl_gc_disable_finalizers_internal)
+#ifdef NDEBUG
+             || is_libjulia_func(jl_gc_enable_finalizers_internal)
+#endif
+             ) {
+        JL_GC_POP();
+        Value *ptls_i32 = emit_bitcast(ctx, ctx.ptlsStates, T_pint32);
+        const int finh_offset = offsetof(jl_tls_states_t, finalizers_inhibited);
+        Value *pfinh = ctx.builder.CreateInBoundsGEP(ptls_i32, ConstantInt::get(T_size, finh_offset / 4));
+        LoadInst *finh = ctx.builder.CreateAlignedLoad(pfinh, Align(sizeof(int32_t)));
+        if (is_libjulia_func(jl_gc_disable_finalizers_internal))
+            ctx.builder.CreateStore(ctx.builder.CreateAdd(finh, ConstantInt::get(T_int32, 1)), pfinh);
+        else
+            ctx.builder.CreateStore(ctx.builder.CreateSub(finh, ConstantInt::get(T_int32, 1)), pfinh);
+        return ghostValue(jl_nothing_type);
+    }
     else if (is_libjulia_func(jl_get_current_task)) {
         assert(lrt == T_prjlvalue);
         assert(!isVa && !llvmcall && nccallargs == 0);
